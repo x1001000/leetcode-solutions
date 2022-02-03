497e0686c79b66cd6e45ccb6e5a65a3f6571d890
十百千 <x1001000@users.noreply.github.com>
2022-02-03 17:43:08 +0800

    Update 202-happy-number.py

--- 202-happy-number.py
+++ 202-happy-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        history = []
-        while n not in history and n != 1:
-            history.append(n)
+        cycle = []
+        while n not in cycle and n != 1:
+            cycle.append(n)
             n = sum(map(lambda x: int(x)**2, str(n)))
         return True if n == 1 else False
